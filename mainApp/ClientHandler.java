5ad7aac2f68c6bcd9538760aa2fe6c4cd827eb7d
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-10-17 21:02:53 +0200

    Implementing handling of messages
    I'm done with message reading on the server side. I now need to implement a better "protocol" for communications, check the content of message received and send the correct answer.

--- mainApp/ClientHandler.java
+++ mainApp/ClientHandler.java
@@ -13,11 +13,18 @@ public class ClientHandler extends Thread {
     If it receives one, it sends one to everyone        */
   public void run() {
     int input;
-    byte[] mybyte = new byte[7]; //TODO magic number should change that
     try {
       DataInputStream in = new DataInputStream(client.getInputStream());
-      do { 
-        in.read(mybyte); 
+      do {
+        /*Discard the first byte*/
+        in.readByte();
+        int length = readLength(in);
+        if(length == -1)
+          throw new IOException();
+
+        byte[] mask = readMask(in);
+        byte[] data = readData(length, mask, in);
+        print(data);
         broadcast();
       } while(!client.isClosed());
     } catch(IOException e) {
@@ -29,6 +36,62 @@ public class ClientHandler extends Thread {
         Server.connections.remove(client);
       }catch(Exception e1) {};
     }
+  }
+
+  private void print(byte[] b) {
+    System.out.println("data has size: "+b.length);
+    System.out.print("{");
+    for(int i = 0; i < b.length; i++) {
+      System.out.print(b[i] + "; ");
+    }
+    System.out.println("}");
+  }
+  
+  private int byteToUnsigned(byte b) {
+    return b & 0xFF;
+  }
+
+  private int unmask(byte b) {
+    return b & 0x7F;
+  }
+
+  /*Reads the length of the data
+    Throwing enable clientHandler to know something is wrong
+    And kill the client*/
+  private int readLength(DataInputStream in) throws IOException {
+    if(in == null)
+      return -1;
+    
+    int b = unmask(in.readByte());
+    /*Length fits in one byte*/
+    if(b < 126) 
+      return b; 
+     
+    /*We do not handle larger sizes*/
+    return -1;            
+  }
+
+  /*Reads the mask in the data*/
+  private byte[] readMask(DataInputStream in) throws IOException {
+    if(in == null)
+      return null;
+    
+    byte[] mask = new byte[4];
+    in.read(mask);
+    return mask;
+  }
+  
+  /*Reads the data and unmasks it*/
+  private byte[] readData(int length, byte[] mask, DataInputStream in) throws IOException {
+    if(length < 0 || in == null || mask == null || mask.length != 4)
+      return null;
+    
+    byte[] data = new byte[length];
+    in.read(data);
+    for(int i = 0; i < length; i++) { 
+      data[i] = (byte) (data[i] ^ mask[i]);
+    }
+    return data;
   }
 
   /*Broadcast message to the other clients*/
